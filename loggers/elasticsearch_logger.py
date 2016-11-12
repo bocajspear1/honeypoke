5d25a614d38f0bfda389e5e3f2812cfb106b6331
Jacob Hartman <junyunharold1@gmail.com>
2016-11-12 15:59:42 -0500

    Changes for elasticsearch and SSL

--- loggers/elasticsearch_logger.py
+++ loggers/elasticsearch_logger.py
@@ -1,14 +1,20 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 import datetime
 
 class ElasticSearchLogger(object):
 
     def __init__(self, config):
         self.__server = config['server']
+        if "https:" in self.__server:
+            self.__use_ssl = True
+        else:
+            self.__use_ssl = False
+
+        self.__verify_certs = config['verify']
 
     
     def log(self, remote_ip, remote_port, protocol, port, data, is_binary):
-        es = Elasticsearch(self.__server)
+        es = Elasticsearch(self.__server, use_ssl=self.__use_ssl, verify_certs=self.__verify_certs)
 
         es.index(index='honeypoke', doc_type='connection', body={
             "time": datetime.datetime.now().isoformat(),
